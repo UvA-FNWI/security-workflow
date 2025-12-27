50ec9452d81cd9f7823fc18a0a49abc985369adc
Gerrit Oomens <g.oomens@uva.nl>
2025-12-27 19:50:33 +0100

    Implement messages

--- Messages/MessagesController.cs
+++ Messages/MessagesController.cs
@@ -1,20 +1,89 @@
 using Microsoft.AspNetCore.Mvc;
+using UvA.Workflow.Users;
 
 namespace UvA.Workflow.Security.Messages;
 
 [ApiController]
 [Route("[controller]")]
-public class MessagesController(MessagesRepository repository) : ControllerBase
+public class MessagesController(MessagesRepository repository, IUserRepository userRepository,
+    IUserService userService) : ControllerBase
 {
     [HttpGet("{instanceId}")]
     public async Task<IEnumerable<MessageDto>> GetMessages(string instanceId, CancellationToken ct)
     {
         var messages = await repository.GetMessages(instanceId, ct);
-        return messages.Select(MessageDto.Create);
+        return await ToDto(messages, ct);
+    }
+
+    private async Task<IEnumerable<MessageDto>> ToDto(ICollection<Message> messages, CancellationToken ct)
+    {
+        var userIds = messages.SelectMany(m => m.Items).Select(i => i.AuthorId).Distinct();
+        var users = (await userRepository.GetByIds(userIds.ToList(), ct)).ToDictionary(u => u.Id);
+        
+        return messages.Select(m => MessageDto.Create(m, users));
+    }
+
+    [HttpPost("{instanceId}")]
+    public async Task<MessageDto> AddMessage(string instanceId, MessageInput input, CancellationToken ct)
+    {
+        var user = await userService.GetCurrentUser(ct);
+        if (user == null) throw new UnauthorizedAccessException();
+
+        var item = new MessageItem
+        {
+            AuthorId = user.Id,
+            DateTime = DateTime.Now,
+            Kind = input.Kind,
+            Body = input.Body
+        };
+
+        Message? message;
+        if (input.ReplyToId != null)
+        {
+            message = await repository.AddMessageItem(instanceId, input.ReplyToId, item, ct);
+            if (message == null) throw new InvalidOperationException();
+        }
+        else
+        {
+            message = new Message
+            {
+                InstanceId = instanceId,
+                QuestionName = input.QuestionName,
+                Items = [item]
+            };
+            await repository.AddMessage(message, ct);
+        }
+        return (await ToDto([message], ct)).First();
     }
 }
 
-public record MessageDto(string Id)
+public record MessageInput(
+    string? QuestionName,
+    string? ReplyToId,
+    ItemKind Kind = ItemKind.Message,
+    string? Body = null
+);
+
+public record MessageDto(string Id, string? QuestionName, bool IsClosed, MessageItemDto[] Items)
 {
-    public static MessageDto Create(Message message) => new(message.Id);
+    public static MessageDto Create(Message message, Dictionary<string, User> users) => new(
+        message.Id,
+        message.QuestionName,
+        message.Items.LastOrDefault()?.Kind == ItemKind.Close,
+        message.Items.Select(i => MessageItemDto.Create(i, users)).ToArray()    
+    );
+}
+
+public record UserDto(string Id, string DisplayName)
+{
+    public static UserDto Create(User user) => new(user.Id, user.DisplayName);
+}
+
+public record MessageItemDto(DateTime DateTime, UserDto User, string? Body)
+{
+    public static MessageItemDto Create(MessageItem item, Dictionary<string, User> users) => new(
+        item.DateTime, 
+        UserDto.Create(users[item.AuthorId]), 
+        item.Body
+    );
 }
--- Messages/MessagesRepository.cs
+++ Messages/MessagesRepository.cs
@@ -8,15 +8,42 @@ public class MessagesRepository(IMongoDatabase database)
 {
     private readonly IMongoCollection<Message> _messagesCollection =
         database.GetCollection<Message>("messages");
+
+    private static FilterDefinitionBuilder<Message> Filter => Builders<Message>.Filter; 
     
     public async Task Create(Message message, CancellationToken ct) 
         => await _messagesCollection.InsertOneAsync(message, cancellationToken: ct);
 
-    public async Task<IEnumerable<Message>> GetMessages(string instanceId, CancellationToken ct)
+    public async Task<ICollection<Message>> GetMessages(string instanceId, CancellationToken ct)
     {
+        if (!ObjectId.TryParse(instanceId, out var instanceObjectId))
+            return [];
+        
         var cursor = await _messagesCollection
-            .FindAsync(m => m.InstanceId == instanceId, cancellationToken: ct) ;
+            .FindAsync(Filter.Eq("InstanceId", instanceObjectId), cancellationToken: ct) ;
         return await cursor.ToListAsync(ct);
+    }
+    
+    public async Task<Message?> AddMessageItem(string instanceId, string messageId,
+        MessageItem item, CancellationToken ct)
+    {
+        if (!ObjectId.TryParse(messageId, out var objectId) || !ObjectId.TryParse(instanceId, out var instanceObjectId))
+            return null;
+        
+        var message = await _messagesCollection.FindOneAndUpdateAsync(
+            Filter.And(Filter.Eq("InstanceId", instanceObjectId), Filter.Eq("_id", objectId)),
+            Builders<Message>.Update.Push(m => m.Items, item),
+            cancellationToken: ct
+        );
+        
+        message.Items = message.Items.Append(item).ToArray();
+        
+        return message;
+    }
+    
+    public async Task AddMessage(Message message, CancellationToken ct)
+    {
+        await _messagesCollection.InsertOneAsync(message, cancellationToken: ct);
     }
 }
 
@@ -40,5 +67,13 @@ public class MessageItem
     [BsonRepresentation(BsonType.ObjectId)]
     public string AuthorId { get; set; } = null!;
 
-    public string Body { get; set; } = null!;
+    public string? Body { get; set; }
+    
+    public ItemKind Kind { get; set; }
+}
+
+public enum ItemKind
+{
+    Message,
+    Close
 }
